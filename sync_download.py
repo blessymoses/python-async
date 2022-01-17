a819f8017be185baa7b8d4225e2c807a338e07b3
blessymoses <blessymoses17@gmail.com>
2022-01-17 22:37:40 +0800

    sync download

--- sync_download.py
+++ sync_download.py
@@ -15,3 +15,13 @@ def download_all_sites(sites):
     with requests.Session() as session:
         for url in sites:
             download_site(url, session)
+
+
+if __name__ == "__main__":
+    sites = ["https://github.com/blessymoses", "https://testdriven.io"] * 100
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(
+        f"Downloaded {len(sites)} sites in {duration} seconds"
+    )  # Downloaded 200 sites in 44.77229833602905 seconds
