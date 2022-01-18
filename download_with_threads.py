ad89018fd6993f328663e5b62128961ca0f5a364
blessymoses <blessymoses17@gmail.com>
2022-01-18 22:49:26 +0800

    download sites using threads & concurrent.futures

--- download_with_threads.py
+++ download_with_threads.py
@@ -6,7 +6,6 @@ import threading
 import time
 
 import requests
-from requests.sessions import session
 
 thread_local = threading.local()
 
@@ -19,3 +18,20 @@ def get_session():
 
 def download_site(url):
     session = get_session()
+    with session.get(url) as response:
+        print(f"Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites)
+
+
+if __name__ == "__main__":
+    sites = ["https://github.com/blessymoses", "https://testdriven.io"] * 100
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(
+        f"Downloaded {len(sites)} sites in {duration} seconds"
+    )  # Downloaded 200 sites in 12.291815996170044 seconds
